9705c96cc888a27457cf14feea8d7435c422d9fd
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-29 13:09:36 -0700

    Update school to pass test_it_exists

---
+++ lib/school.rb
@@ -0,0 +1,9 @@
+class School
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+
+end
